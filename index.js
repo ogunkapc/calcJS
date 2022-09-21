8e3df3bd3ff89db667bb39b2e4f01d7d17d43409
ogunkapc <ogunkapc@gmail.com>
2022-09-22 00:48:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// let operation = prompt(
+//   "What operation would you like to perform?\nChoose from the following:\n'+' for addition\n'-' for subtraction\n'*' for multiplication\n'/' for division\n'%' for modulo division"
+// );
+// let num1 = prompt("Enter the first number: ");
+// let num2 = prompt("Enter the second number");
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (operation == "+") {
+//   var sum = num1 + num2;
+//   alert(num1 + " + " + num2 + " = " + sum);
+// } else if (operation == "-") {
+//   var difference = num1 - num2;
+//   alert(num1 + " - " + num2 + " = " + difference);
+// } else if (operation == "*") {
+//   var product = num1 * num2;
+//   alert(num1 + " * " + num2 + " = " + product);
+// } else if (operation == "/") {
+//   var quotient = num1 / num2;
+//   alert(num1 + " / " + num2 + " = " + quotient);
+// } else if (operation == "%") {
+//   var modulos = num1 % num2;
+//   alert(num1 + " % " + num2 + " = " + modulos);
+// } else {
+//   alert("INVALID OPERATION");
+// }
